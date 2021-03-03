3c349b21892e2b2dedec9c179617b94453bd2785
mamudsamiul <samiulmamud1995@gmail.com>
2021-03-03 11:21:05 +0530

    (Samiul Mamud) Created: API Gateway is created

---
+++ src/main/java/com/capgemini/microservices/cloud/gateway/MicroservicesRegistry1Application.java
@@ -0,0 +1,13 @@
+package com.capgemini.microservices.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesRegistry1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesRegistry1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/microservices/cloud/gateway/MicroservicesRegistry1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.microservices.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesRegistry1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
